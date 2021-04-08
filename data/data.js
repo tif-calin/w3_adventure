98c781ea4512c799a1748fa4e820d31bd4e8eab1
tif-calin <culitif@tuta.io>
2021-04-08 16:26:50 -0700

    set up site structure

---
+++ data/data.js
@@ -0,0 +1,40 @@
+export const quests = {
+    aluminum: {
+        messages: [
+            {
+                prompt: 'Welcome to Suriname!',
+                responses: [
+                    
+                ]
+            },
+            {
+                prompt: 'prompt',
+                responses: [
+
+                ]
+            }
+        ],
+        links: [
+            'https://mineralseducationcoalition.org/minerals-database/aluminum/',
+            'https://en.wikipedia.org/wiki/Aluminium'
+
+        ]
+    },
+    beryllium: {
+        links: [
+            'https://mineralseducationcoalition.org/minerals-database/beryllium/'
+        ]
+    },
+    brass: {},
+    copper: {},
+    gold: {},
+    iron: {},
+    lithium: {},
+    mercury: {},
+    nickel: {},
+    osmium: {},
+    silver: {},
+    titanium: {},
+    vibranium: {},
+    zinc: {}
+};

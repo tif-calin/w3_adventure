9a47e56710bdc9a3687df58b25f897fb295b3e0d
tif-calin <culitif@tuta.io>
2021-04-08 21:50:53 -0700

    data model

--- data/data.js
+++ data/data.js
@@ -1,19 +1,22 @@
 export const quests = {
     aluminum: {
-        messages: [
-            {
-                prompt: 'Welcome to Suriname!',
-                responses: [
-                    
-                ]
+        start: 'welc',
+        messages: {
+            'welc': {
+                prompt: 'You step off the plane into beautiful Suriname where aluminum bauxite ore is the number one export.',
+                responses: {
+                    'response1': 'aaa2',
+                    'response2': 'aaa2'
+                }
             },
-            {
+            'aaa2': {
                 prompt: 'prompt',
-                responses: [
-
-                ]
+                responses: {
+                    'response1': null,
+                    'response2': 'welc'
+                }
             }
-        ],
+        },
         links: [
             'https://mineralseducationcoalition.org/minerals-database/aluminum/',
             'https://en.wikipedia.org/wiki/Aluminium'
---
+++ map/map.js
@@ -0,0 +1,18 @@
+import { quests } from '../data/data.js';
+
+const questList = document.querySelector('.quest-list');
+
+const initialize = () => {
+    questList.innerHTML = '';
+    for (let quest of Object.keys(quests)) {
+        const item = document.createElement('li');
+        const anchor = document.createElement('a');
+
+        anchor.href = `../quest/?=${quest}`;
+        anchor.textContent = quest;
+        item.appendChild(anchor);
+        questList.appendChild(item);
+    }
+};
+
+initialize();

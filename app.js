eef8440dc4fdbab40f20d7c4044476541c5a45c7
tif-calin <culitif@tuta.io>
2021-04-12 23:50:16 -0700

    disable input once user is created

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const spnNames = document.querySelectorAll('.name');
 
 // set event listeners to update state and DOM
 inpName.addEventListener('keypress', event => {
-    if (event.key === 'Enter') createUser(inpName.value);
+    if (event.key === 'Enter') {
+        createUser(inpName.value);
 
-    for (let span of spnNames) span.innerHTML = inpName.value;
+        for (let span of spnNames) span.innerHTML = inpName.value;
+    
+        for (let hidden of document.querySelectorAll('.hidden')) {
+            hidden.classList.remove('hidden');
+        }
 
-    for (let hidden of document.querySelectorAll('.hidden')) {
-        console.log(hidden);
-        hidden.classList.remove('hidden');
+        inpName.disabled = true;
     }
 })

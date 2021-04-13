15f4151c1c3926f8103f1fed505a23f472f20f18
tif-calin <culitif@tuta.io>
2021-04-13 08:18:18 -0700

    add comments to init func

--- map/map.js
+++ map/map.js
@@ -3,19 +3,28 @@ import quests from '../data/data.js';
 const questList = document.querySelector('.quest-list');
 
 const initialize = () => {
+    // reload list of quests
     questList.innerHTML = '';
+
+    // loop through quests
     for (let quest of Object.keys(quests)) {
+        // create li
         const item = document.createElement('li');
         item.classList.add('quest');
-        const anchor = document.createElement('a');
 
+        // create <a> with href
+        const anchor = document.createElement('a');
         anchor.href = `../quest/?id=${quest}`;
         anchor.textContent = quest;
         item.appendChild(anchor);
+
+        // only add link if quest was actually implemented
         if (!quests[quest].implemented) {
             item.classList.add('unimplemented');
             item.textContent += ' (not yet implemented)';
         }
+
+        // display the quest
         questList.appendChild(item);
     }
 };

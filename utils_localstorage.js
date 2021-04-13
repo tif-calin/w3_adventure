0f92bc21748adec9649295888e86f94962af565b
tif-calin <culitif@tuta.io>
2021-04-12 23:01:30 -0700

    add localstorage utils

---
+++ utils_localstorage.js
@@ -0,0 +1,21 @@
+const USER = 'USER';
+
+function saveToLocal(key, val) {
+    localStorage.setItem(key, JSON.stringify(val));
+}
+
+export function createUser(name) {
+    const user = {
+        name = name,
+        inventory = {}
+    }
+
+    saveToLocal(USER, user);
+}
+
+export function completeQuest(metal) {
+    const user = localStorage.getItem(USER);
+    user.inventory[metal] = true;
+
+    saveToLocal(USER, user);
+}
